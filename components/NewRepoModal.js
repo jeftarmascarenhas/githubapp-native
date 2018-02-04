032c0461c676828af630c004c31ce5f757479ba3
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2018-02-04 14:33:57 -0300

    init commit

---
+++ components/NewRepoModal.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+
+export default class NewRepoModal extends Component {
+  state = {
+    newRepoText: '',
+  };
+  render() {
+    return (
+      <Modal animationType="fade" transparent={true} visible={this.props.visible} >
+        <View style={styles.modalContainer} >
+          <View style={styles.boxContainer} >
+            <Text>Adicionar Repositório</Text>
+            <TextInput
+              autoFocus
+              autoCapitalize="none"
+              style={styles.boxInput}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              placeholder="organização/repositório"
+              value={this.state.newRepoText}
+              onChangeText={newRepoText => this.setState({ newRepoText })}
+            />
+            <View style={styles.buttonContainer} >
+              <TouchableOpacity
+                style={[styles.button, styles.cancelButton]}
+                onPress={this.props.onCancel}
+              >
+                <Text style={styles.textButton} >Cancelar</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[styles.button, styles.submitButton]}
+                onPress={() => this.props.onAdd(this.state.newRepoText)}
+              >
+                <Text style={styles.textButton} >Adicionar</Text>
+
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxContainer: {
+    padding: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    alignItems: 'center',
+    width: 280,
+  },
+  boxInput: {
+    alignSelf: 'stretch',
+    marginTop: 10,
+    paddingVertical: 0,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    height: 40,
+    borderRadius: 3,
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 40,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  cancelButton: {
+    backgroundColor: '#E25F5F',
+    marginRight: 5,
+  },
+  submitButton: {
+    backgroundColor: '#70BD85',
+  },
+  textButton: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 12,
+  },
+});
---
+++ components/Repo.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  PixelRatio
+} from 'react-native';
+
+export default class Repo extends Component {
+  render() {
+    return (
+      <View style={styles.repo} >
+        <Image
+          style={styles.repoImage}
+          source={{ uri: this.props.data.thumbnail }}
+        />
+        <View style={styles.repoInfo}>
+          <Text style={styles.repoTitle}>{this.props.data.title}</Text>
+          <Text style={styles.repoAuthor}>{this.props.data.author}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({  
+  repo: {
+    padding: 20,
+    borderBottomColor: 'rgba(0,0,0,0.2)',
+    borderBottomWidth: 1 / PixelRatio.get(),
+    marginBottom: 20,
+    borderRadius: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  repoImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+  },
+  repoInfo: {
+    marginLeft: 10,
+  },
+  repoTitle: {
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  repoAuthor: {
+    fontSize: 12,
+    color: '#999',
+  },
+})
